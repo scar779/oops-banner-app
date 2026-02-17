c6d1d781440a1b9449cc6609758119dddee19e65
scar779 <76441862+scar779@users.noreply.github.com>
2026-02-17 14:53:45 +0530

    Initial commit

---
+++ HelloWorld.java
@@ -0,0 +1,21 @@
+<<<<<<< HEAD
+public class HelloWorld{
+
+    public static void main(String[]args){
+
+        System.out.println("Hello World!");
+        
+        
+    }
+}
+=======
+public class HelloWorld{
+
+    public static void main(String[]args){
+
+        System.out.println("Hello World!");
+        
+        
+    }
+}
+>>>>>>> 6dfb2c2 (First commit)
